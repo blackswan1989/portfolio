15e17dacd98216119e449cfd12f6fc37d4e0fe92
blackswan1989 <67410919+blackswan1989@users.noreply.github.com>
2020-11-02 14:56:09 +0900

    JS | Handle scrolling when click & CSS retouch

--- portfolio_v1/js/main.js
+++ portfolio_v1/js/main.js
@@ -5,9 +5,9 @@ console.log("Hello, I'm Front-end Developer HJ")
 
 // navbar transparent when it is on the top
 const navbar = document.querySelector("#navbar");
-const navbarHeigt = navbar.getBoundingClientRect().height
+const navbarHeigt = navbar.getBoundingClientRect().height;
 
-document.addEventListener("scroll", () => {
+window.addEventListener("scroll", () => {
   // console.log(window.scrollY);
   // console.log(`navbar height : ${navbarHeigt}`);
   if(window.scrollY > navbarHeigt) {
@@ -16,3 +16,19 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove('navbar--dark');
   }
 })
+
+// Handle scrolling when click on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link; // 클릭한 data-link를 link로 정의
+  if(link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: "smooth"});
+});
+
