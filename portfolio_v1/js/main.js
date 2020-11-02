18918f526a3597a09204f365e66969b7dd6b2a51
blackswan1989 <67410919+blackswan1989@users.noreply.github.com>
2020-11-02 13:18:40 +0900

    JS | navbar scroll event

--- portfolio_v1/js/main.js
+++ portfolio_v1/js/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log("Hello, My future coworker?")
+console.log("Hello, I'm Front-end Developer HJ")
+
+
+// navbar transparent when it is on the top
+const navbar = document.querySelector("#navbar");
+const navbarHeigt = navbar.getBoundingClientRect().height
+
+document.addEventListener("scroll", () => {
+  // console.log(window.scrollY);
+  // console.log(`navbar height : ${navbarHeigt}`);
+  if(window.scrollY > navbarHeigt) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+})
