902eda8dc002413337fb420ab2e920d82dc91668
blackswan1989 <67410919+blackswan1989@users.noreply.github.com>
2020-11-02 15:51:49 +0900

    JS | #home contact me btn - click event

--- portfolio_v1/js/main.js
+++ portfolio_v1/js/main.js
@@ -28,7 +28,19 @@ navbarMenu.addEventListener("click", (event) => {
   }
   console.log(event.target.dataset.link);
 
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: "smooth"});
+  scrollIntoView(link);
 });
 
+
+// Handle click on "contact me" button on home
+
+const homeContactMeBtn = document.querySelector(".home__contact");
+
+homeContactMeBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+})
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: "smooth"});
+}
