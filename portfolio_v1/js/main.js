2e1613f0908c507ade44d97c1fbce76c0a92f1cf
blackswan1989 <67410919+blackswan1989@users.noreply.github.com>
2020-11-04 14:04:36 +0900

    js | safari smooth scrolling -  delete

--- portfolio_v1/js/main.js
+++ portfolio_v1/js/main.js
@@ -12,24 +12,6 @@ function scrollIntoView(selector) {
   scrollTo.scrollIntoView({behavior: "smooth"});
 }
 
-function SmoothVerticalScrolling(e, time, where) {
-  var eTop = e.getBoundingClientRect().top;
-  var eAmt = eTop / 100;
-  var curTime = 0;
-  while (curTime <= time) {
-      window.setTimeout(SVS_B, curTime, eAmt, where);
-      curTime += time / 100;
-  }
-}
-
-function SVS_B(eAmt, where) {
-  if(where == "center" || where == "")
-      window.scrollBy(0, eAmt / 2);
-  if (where == "top")
-      window.scrollBy(0, eAmt);
-}
-
-SmoothVerticalScrolling(navbar, 275, "center");
 
 window.addEventListener("scroll", () => {
   // console.log(window.scrollY);
