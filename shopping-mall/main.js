3577b380dec64b0a4735cb303a6ae5de92ab69f3
blackswan1989 <67410919+blackswan1989@users.noreply.github.com>
2020-11-12 16:31:29 +0900

    JS: console , back to btn, menu toggle

---
+++ shopping-mall/main.js
@@ -0,0 +1,43 @@
+"use strict";
+
+console.log(
+  "%c Hello, I'm Front-end Developer HyoJin ",
+  "color:black; font-size:14px; background:white"
+);
+console.log(
+  "%c If you are interested in me, please contact me. ",
+  "color:black; font-size:14px; background:white"
+);
+console.log(
+  "%c My cell phone number is ",
+  "color:black; font-size:14px; background:white"
+);
+console.log(
+  "%c 010 7194 3925 ",
+  "color:black; font-size:16px; background:white"
+);
+console.log(
+  "%c Thank you, have a nice day. ",
+  "color:black; font-size:14px; background:white"
+);
+
+// Back to top Btn
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
+
+const backToTop = document.querySelector(".back-button__btn");
+
+backToTop.addEventListener("click", () => {
+  scrollIntoView(".header");
+});
+
+// Menu button toggle
+const navbarMenu = document.querySelector(".tg-active");
+
+const navbarToggleBtn = document.querySelector(".toggle-btn");
+
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("toggle-active");
+});
